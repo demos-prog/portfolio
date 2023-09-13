ef7426da2484c4a9f31692d8fcd192016b1980af
Дмитрий <demosne@gmail.com>
2023-09-13 11:56:40 +0300

    about section

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 "use strict"
+
+const skills_elem = document.querySelector('#skills');
+
+const skills = ["HTML", "CSS", "JavaScript", "React", "Java", "Salesforse", "English: intermediate (B1)"];
+
+skills.forEach(skill => {
+  const div = document.createElement('div');
+  div.className = "skill";
+  div.innerText = skill;
+  skills_elem.append(div);
+});
