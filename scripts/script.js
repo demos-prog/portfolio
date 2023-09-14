7a0c48df50abd5f32cd45f1c18e209945e223031
Дмитрий <demosne@gmail.com>
2023-09-14 08:58:19 +0300

    fixing

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,21 @@
 "use strict"
 
+// showing skills
 const skills_elem = document.querySelector('#skills');
-
 const skills = ["HTML", "CSS", "JavaScript", "React", "Java", "Salesforse", "English: intermediate (B1)"];
-
 skills.forEach(skill => {
   const div = document.createElement('div');
   div.className = "skill";
   div.innerText = skill;
   skills_elem.append(div);
 });
+
+// closing burger menu after selecting section
+const menu_buttons = document.querySelectorAll('.menu_button');
+const checkbox = document.querySelector('.checkbox');
+
+menu_buttons.forEach(el => {
+  el.addEventListener('click', () => {
+    if (checkbox.checked) checkbox.checked = false;
+  })
+})
